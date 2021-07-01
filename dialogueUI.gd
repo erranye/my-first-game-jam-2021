c4fb03eb251646aae290310abd5b8878afbff72c
erranye <erranye@gmail.com>
2021-07-01 03:49:24 -0400

    Dialogue UI text animations, added ability to skip dialogue animation + skip dialogue

--- dialogueUI.gd
+++ dialogueUI.gd
@@ -1,14 +1,43 @@
 extends Control
 
-onready var dialogue_container = $DialogueContainer
-onready var speaker_name = $DialogueContainer/speakerName
-onready var speaker_dialogue = $DialogueContainer/DialogueBox/speakerDialogue
+var dialogue_container
+var speaker_name
+var speaker_dialogue
+var tween
+var animating = true
 
+func _ready():
+	dialogue_container = $DialogueContainer
+	speaker_name = $DialogueContainer/speakerName
+	speaker_dialogue = $DialogueContainer/DialogueBox/speakerDialogue
+	tween = $DialogueContainer/DialogueBox/Tween
+	
+	speaker_dialogue.percent_visible = 0
+
+func tween_dialogue():
+	# Reset typing animation
+	speaker_dialogue.percent_visible = 0
+	# Tween interpolate property to change "percent_visible" from 0 to 1 over 2 seconds
+	tween.interpolate_property(speaker_dialogue, "percent_visible", 0, 1, 2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+	
+func display_all_dialogue():
+	tween.stop_all()
+	speaker_dialogue.percent_visible = 1
+
+func all_dialogue_visible():
+	return speaker_dialogue.percent_visible == 1
 
 func set_speaker_name(new_name):
 	speaker_name.text = new_name
 	
 func set_dialogue_text(text):
 	speaker_dialogue.bbcode_text = text
+	
+func show_text_box():
+	dialogue_container.visible = true
+	
+func hide_text_box():
+	dialogue_container.visible = false
 
 
--- scenes/dialogueTest.gd
+++ scenes/dialogueTest.gd
@@ -7,19 +7,23 @@ var scene_dialogue
 var dialogue_ui
 
 func _ready():
-	scene_name = "test_scene"
+	scene_name = get_tree().get_current_scene().get_name()
 	scene_dialogue_handler = dialogue_handler.new()
 	scene_dialogue = scene_dialogue_handler.load_dialogue("res://test.json", scene_name)
 	dialogue_ui = $DialogueUI
 	update_dialogue()
 
 func _physics_process(delta):
-	if scene_dialogue_handler.dialogue_active:
-		if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept"):
+		if dialogue_ui.all_dialogue_visible():
 			if scene_dialogue_handler.dialogue_finished:
 				pass
+				# Scene transition
 			else:
 				update_dialogue()
+		else:
+			dialogue_ui.display_all_dialogue()
+				
 	
 
 func update_dialogue():
@@ -27,4 +31,6 @@ func update_dialogue():
 	print(scene_dialogue_handler.dialogue_index)
 	dialogue_ui.set_speaker_name(scene_dialogue_handler.current_speaker)
 	dialogue_ui.set_dialogue_text(scene_dialogue_handler.current_speaker_text)
+	dialogue_ui.tween_dialogue()
+	
 	
